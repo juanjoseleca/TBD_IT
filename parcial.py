a251f58392904ea2047902c7bc7b42d70a38a9eb
juan <jjleoncamilo@gmail.com>
2019-05-03 00:28:21 -0500

    Corregido y con vecinos cercanos

---
+++ parcial.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import math
+from csv_splitter import split
+def Coseno(x,y):
+    if(len(x)!=len(y)):
+        print("error de tamaño")
+        return
+    sum1=0
+    sum2=0
+    sum3=0
+    n=len(x)
+    for i in range(n):
+        sum1+=x[i]**2
+        sum2+=y[i]**2
+        sum3+=x[i]*y[i]
+    return sum3/(math.sqrt(sum1)*math.sqrt(sum2))
+class RecommenderSystem:
+    def __init__(self,location):
+        self.location=location
+        
--- recommender_system.py
+++ recommender_system.py
@@ -3,7 +3,9 @@ import gc
 import time
 import math
 from scipy import spatial
+import numpy as np
 f = open("tams.txt", "w")
+MAX_VECINOS=10
 def cargar(num):
         nombre_archivo=str(num)+".csv"
         archivo = pd.read_csv(
@@ -22,22 +24,68 @@ def cargar(num):
 def get_ratings_user(user_id):
         nro_archivo=math.ceil(user_id/8000)
         mi_matrix=cargar(nro_archivo)
-        data=mi_matrix.loc[user_id]
+        data=mi_matrix.loc[user_id].values
+        #print(data)
         del mi_matrix
         gc.collect()
         return data
-def get_distance(array_user,array_files):
+def get_distance(user_id,array_user,array_files):
+        #final_list=np.empty(0)
+        final_list_valor=[]
+        final_list_ids=[]
         for i in array_files:
-                results = np.zeros(8000)
+                if(i==1):
+                        results = np.zeros(7999)
+                        ids = np.zeros(7999)
+                else:
+                        results = np.zeros(8000)
+                        ids = np.zeros(8000)
                 mi_matrix=cargar(i)
                 for i in range(len(mi_matrix.index)):
-                        if(userIdRecived!=mi_matrix.index[i]):
+                        if(user_id!=mi_matrix.index[i]):
                                 array_=mi_matrix.iloc[i].values
-                                results[i]=1-spatial.distance.cosine(otro,array_)
+                                tam_1=len(array_)
+                                tam_2=len(array_user)
+                                if(tam_1!=tam_2):
+                                        difference=abs(tam_1-tam_2)
+                                        list_n=np.zeros(difference)
+                                        #print(type(list_n))
+                                        if(tam_1<tam_2):
+                                                array_=np.concatenate((array_,list_n))
+                                        else:
+                                                array_user=np.concatenate((array_user,list_n))
+                                #print(len(array_)," vs ",len(array_user))
+                                ids[i]=mi_matrix.index[i]
+                                #print(ids[i])
+                                results[i]=1-spatial.distance.cosine(array_user,array_)
                         else:
-                                results[i]=0 
-    #return matrix
+                                ids[i]=mi_matrix.index[i]
+                                results[i]=0
+                ordenada=sorted(range(len(results)), key=lambda k: results[k],reverse=True)
+                ordenada=ordenada[:10]
+                to_send_valor=[]
+                to_send_id=[]
+                for i in ordenada:
+                        to_send_valor.append(results[i])
+                        to_send_id.append(ids[i])
+                #print("->>>>",to_send_id)
+                #final_list=np.concatenate((final_list,results))
+                del results
+                del ids
+                gc.collect() 
+                final_list_valor=final_list_valor+to_send_valor
+                final_list_ids=final_list_ids+to_send_id
+        return final_list_valor,final_list_ids
+def vecinos_cercanos(user_id):
+        lista,ids=get_distance(user_id,get_ratings_user(user_id),list(range(1,19)))
+        return lista,ids
 seconds = time.time()
-print(get_ratings_user(82317))
+valores,ids=vecinos_cercanos(82317)
+#print(ids)
+final_ordenada=sorted(range(len(valores)), key=lambda k: valores[k],reverse=True)
+final_ordenada=final_ordenada[0:10]
+for i in final_ordenada:
+        print("ID: ",int(ids[i])," Valor: ",valores[i])
+#print(final_ordenada)
 now_seconds = time.time()
-print(now_seconds-seconds)
+print("Tiempo de ejecucion: ",now_seconds-seconds)
